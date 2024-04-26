0b15984327464fc1763b358a522f4fd7f9cfdf13
Mohd Rizwaan Ansari <134074832+wrizzzlerrr@users.noreply.github.com>
2024-04-26 23:42:44 +0530

    push 1

---
+++ src/Components/Meal.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import MealItem from "./MealItem";
+import ReacipeIndex from "./RecipeIndex";
+const Meal = () => {
+    const [search,setSearch]=useState();
+    const [show,setShow]=useState(false);
+    const [item,setItem]=useState("");
+    const [url,setUrl]=useState("https://www.themealdb.com/api/json/v1/1/search.php?f=a");
+   
+    useEffect(()=>{
+        fetch(url).then(res=>res.json()).then(data=> {
+            setItem(data.meals);
+            setShow(true);
+        })
+     },[url])
+
+     const searchRecipe=(evt)=>{
+         setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+    }
+    const setIndex=(alpha)=>{
+        setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`);
+    }
+return (
+    <>
+        <div className="main">
+            <div className="heading">
+                <h1>VIT FOOD MALL</h1>
+            </div>
+            <div className="searchBox">
+                <input placeholder="Search Your Recipe.." type="search" className="search-bar" onChange={e=> setSearch(e.target.value)} onKeyPress={searchRecipe}/>
+            </div>
+            <div className="container">
+                {
+                    show ?<MealItem data={item} /> :"Not Found"
+                
+                }
+            </div>
+            <br /><br /><br /><br /><br />
+            <div className="indexContainer">
+                 <ReacipeIndex alphaIndex={(alpha)=>setIndex(alpha)}/>
+            </div>
+            
+        </div>
+    </>
+)
+}
+export default Meal;
